843afc2d2c7fdd07c41df0207535cc21d367e15c
puneethraghusr <53910050+puneethraghusr@users.noreply.github.com>
2020-03-03 13:09:41 +0530

    Add files via upload

---
+++ maxmeeting.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+void printMaxActivities(int s[], int f[], int n)
+{
+    int i, j;
+ 
+    printf ("Following activities are selected n");
+ 
+
+    i = 0;
+    printf("%d ", i);
+ 
+    for (j = 1; j < n; j++)
+    {
+     
+      if (s[j] >= f[i])
+      {
+          printf ("%d ", j);
+          i = j;
+      }
+    }
+}
+ 
+int main()
+{
+int i,n;
+printf("Enter no of tasks");
+scanf("%d",&n);
+    int s[n], f[n];
+   for( i = 0;i<n;i++)
+scanf("%d %d",&s[i],&f[i]);
+    printMaxActivities(s, f, n);
+    return 0;
+} 
